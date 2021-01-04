edc4dc0b4ce007362ec87e859e4930628e7c20de
Azura <zhangxiaze@live.com>
2021-01-04 18:08:43 +0800

    test Serialization

--- DataStructureModule.py
+++ DataStructureModule.py
@@ -1,6 +1,9 @@
 # 本模块定义了数据结构，并实现查询、删除、增加、修改等功能
 import pickle
 from enum import Enum
+from dataclasses import dataclass, field
+from typing import List
+
 # ======================== Const definition ========================
 # MAX Table Name length (bytes)
 MAX_TABLE_NAME_LEN = 64
@@ -23,6 +26,7 @@ class DataType(Enum):
     TEXT = 3
 
 
+@dataclass
 class Field:
     FieldName: str = ''
     FieldType: DataType = DataType.BIN
@@ -30,15 +34,16 @@ class Field:
     FieldAllowNull: bool = True
 
 
+@dataclass
 class Table:
     # Name of the Table
     TableName: str = ''
     # cols in the table
     # Eg: [Field1(Name,TEXT),Field2(Gender,TEXT),Field3(Age,INTERGER)]
-    TableField: list = []
+    TableField: List[Field] = field(default_factory=list)
     # Rows in the table
     # Eg: [(Zhangsan,Male,14),(lisi,Male,16),(Wanghong,Female,15)]
-    TableData: list = []
+    TableData: List[tuple] = field(default_factory=list)
 
 # ======================== Class definition end ========================
 
@@ -54,19 +59,20 @@ Database: list = []
 
 # Open and Read Database File
 def OpenFile(filePath: str) -> int:
-    global DBFileHandle
+    global DBFileHandle, Database
     try:
-        DBFileHandle = open(filePath, 'r+')
+        DBFileHandle = open(filePath, 'rb+')
     except:
         print("Error: 没有找到文件或读取文件失败")
         return 1
     else:
-        pickle.load(DBFileHandle, Database)
+        Database = pickle.load(DBFileHandle)
         return 0
 
 
 def SaveFile() -> int:
     if(DBFileHandle == None):
+        print("Error: 尚未打开文件")
         return 1
     pickle.dump(Database, DBFileHandle)
     return 0
@@ -74,3 +80,30 @@ def SaveFile() -> int:
 
 if __name__ == "__main__":
     OpenFile('database.db')
+    print(Database)
+
+    Database = [Table('Student',
+                      [
+                          Field('Name', DataType.TEXT, False, True),
+                          Field('No', DataType.INTERGER, True, False),
+                          Field('Gender', DataType.TEXT, False, True)
+                      ],
+                      [
+                          ('ZhangSan', 10011, 'M'),
+                          ('LiSi', 10012, 'M'),
+                          ('WangWu', 10013, 'F')
+                      ]
+                      ),
+                Table('Score',
+                      [
+                          Field('No', DataType.INTERGER, True, False),
+                          Field('Score', DataType.INTERGER, False, True)
+                      ],
+                      [
+                          (10011, 89),
+                          (10012, 92),
+                          (10013, 99)
+                      ]
+                      )
+                ]
+    SaveFile()
